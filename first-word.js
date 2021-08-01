f7a14bf01e3fe1a19d1d26d206eafc9ab96ac09e
redcross16 <belkill@mail.ru>
2021-08-01 23:51:54 +0300

    first word checkIo

---
+++ first-word.js
@@ -0,0 +1,18 @@
+import assert from "assert";
+
+function firstWord(text: string): string {
+    // your code here
+    return '';
+}
+
+
+console.log('Example:')
+console.log(firstWord("Hello world"))
+
+// These "asserts" using for self-checking and not for auto-testing
+console.log(firstWord("Hello world"), "Hello")
+console.log(firstWord("don't touch it"), "don't")
+console.log(firstWord("greetings, friends"), "greetings")
+console.log(firstWord("... and so on ..."), "and")
+console.log(firstWord("hi"), "hi")
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
