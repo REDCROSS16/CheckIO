9244a84c61a99f0bce683d0be2b1d2ddbac10fd7
redcross16 <belkill@mail.ru>
2021-11-18 11:39:58 +0300

    123

---
+++ biggerPrice.js
@@ -0,0 +1,38 @@
+interface Stock {
+    name: string,
+    price: number,
+};
+
+function biggerPrice(limit: number, data: Stock[]): Stock[] {
+    // перебираем массив с объектами через сорт
+    let res = [];
+    data.sort((prev,next) => next.price - prev.price);
+    for (let i = 0; i < limit; i++) {
+         res.push(data[i]);  
+    }
+    
+    return res;
+}
+
+console.log('Example:')
+console.log(biggerPrice(2, [
+    {"name": "bread", "price": 100},
+    {"name": "wine", "price": 138},
+    {"name": "meat", "price": 15},
+    {"name": "water", "price": 1}
+]))
+
+assert.deepEqual(biggerPrice(2, [
+    {"name": "bread", "price": 100},
+    {"name": "wine", "price": 138},
+    {"name": "meat", "price": 15},
+    {"name": "water", "price": 1}
+]), [
+    {"name": "wine", "price": 138},
+    {"name": "bread", "price": 100}
+])
+assert.deepEqual(biggerPrice(1, [
+    {"name": "pen", "price": 5},
+    {"name": "whiteboard", "price": 170}
+]), [{"name": "whiteboard", "price": 170}])
+console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
